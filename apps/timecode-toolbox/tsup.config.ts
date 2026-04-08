046e311327485049e7a28cba8861077c37ca7b32
Sam Lanning <sam@samlanning.com>
2026-04-08 10:46:47 +0100

    Allow timecode-toolbox to be run from cli (#18)

--- apps/timecode-toolbox/tsup.config.ts
+++ apps/timecode-toolbox/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/components/frontend/index.tsx'],
+  entry: ['src/index.ts', 'src/start.ts', 'src/components/frontend/index.tsx'],
   format: ['cjs', 'esm'],
   splitting: false,
   dts: true,
