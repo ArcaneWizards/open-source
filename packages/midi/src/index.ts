f2b4870cd1d3aa8063aaa8182d9fa9eb85318541
Sam Lanning <sam@samlanning.com>
2026-05-28 19:56:31 +0100

    Flesh out MIDI Interface

--- packages/midi/src/index.ts
+++ packages/midi/src/index.ts
@@ -1,1 +1,42 @@
-console.log('hello world');
+import { MIDIInterface } from './types.js';
+
+export type {
+  MIDIInterface,
+  MIDIInput,
+  MIDIOutput,
+  MidiEndpointInfo,
+  SupportResponse,
+} from './types.js';
+
+const DEFAULT_MIDI_INTERFACE: MIDIInterface = {
+  getSupportInfo() {
+    return {
+      supported: false,
+      reason: 'MIDI support is not implemented in this environment.',
+    };
+  },
+  getInputs() {
+    return [];
+  },
+  getOutputs() {
+    return [];
+  },
+  openInput() {
+    throw new Error('MIDI support is not implemented in this environment.');
+  },
+  openOutput() {
+    throw new Error('MIDI support is not implemented in this environment.');
+  },
+  createVirtualInput() {
+    throw new Error('MIDI support is not implemented in this environment.');
+  },
+  createVirtualOutput() {
+    throw new Error('MIDI support is not implemented in this environment.');
+  },
+};
+
+export const midi = () => {
+  return DEFAULT_MIDI_INTERFACE;
+};
+
+export default midi;
